d22a00b0b3194a3ba973af76c04bd6487ffecf80
Jacques Grelet <Jacques.grelet@ird.fr>
2018-04-25 17:06:00 +0200

    update TODOS.m

--- TODOS.m
+++ TODOS.m
@@ -9,3 +9,10 @@
 % Works with data fron SBE21, SBE45 or FerryBox
 % Have NetCDF4 representation in memory with group for instruments and
 % processing level (raw, adjusted, ...)
+
+% 04/25/2018
+% test if an tsgqc instance is in memory
+% implement mouse fonction
+% update zoom on axes and map
+% resolve Quality member and reading '+tsgqc\@dynaload\tsgqc_netcdf.csv'
+% implement write for GOSUD, OceanSites and ascii
