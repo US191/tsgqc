1e05100f3ce649efe9eeba48d9d496710074cf8c
Jacques Grelet <Jacques.grelet@ird.fr>
2018-05-22 12:41:53 +0200

    add functionKeyPress and release

--- @tsgqc/private/keyPressFcnCallback.m
+++ @tsgqc/private/keyPressFcnCallback.m
@@ -1,58 +1,40 @@
-% Callback function run when key is pressed
-  function keyPressFcnCallback(obj, evnt)
-    disp(strcat(class(obj),': keyPressFcnCallback callback module not yet implemented'));
+% callback function run when key is pressed
+function keyPressFcnCallback(obj, src, evnt)
+
+% MATLAB generates repeated KeyPressFcn events, desactivate callback
+set(src, 'KeyPressFcn', []);
+
+% check if key is pressed
+if ~isempty(evnt.Key)
+  %
+  % test key, shift or alt/control
+  switch evnt.Key
     
-%     % MATLAB generates repeated KeyPressFcn events, desactivate callback
-%     % ------------------------------------------------------------------
-%     set(hObject, 'KeyPressFcn', []);
-%     
-%     % check if key is pressed
-%     % -----------------------
-%     if ~isempty(evnt.Key)
-%       
-%       % test key, shift or control
-%       % --------------------------
-%       switch evnt.Key
-%         
-%         case 'shift'
-%           
-%           % get current pointer shape
-%           % -------------------------
-%           pointerShape = get(hObject, 'pointer');
-%           
-%           % save current cursor shape
-%           % -------------------------
-%           setappdata( hMainFig, 'tsg_pointer', pointerShape);
-%           
-%           % set cursor to fullcross or reset to normal arrow
-%           % ------------------------------------------------
-%           set(hObject, 'pointer', 'fullcrosshair');
-%           
-%         case 'control'
-%           
-%           % Get current position of cusor and return its coordinates in
-%           % axes
-%           % -----------------------------------------------------------
-%           a = get(hPlotAxes(1), 'CurrentPoint');
-%           x = a(2,1);
-%           
-%           % Test if cursor is inside data interval
-%           % -------------------------------------
-%           if x > tsg.DAYD(1) && x < tsg.DAYD(end)
-%             
-%             % loop over 3 subplot and draw vertical lines
-%             % -------------------------------------------
-%             for iplot = 1:3
-%               axes( hPlotAxes(iplot));
-%               limy = get(hPlotAxes(iplot), 'YLim');
-%               line([x x], limy, 'color', 'k', 'EraseMode', 'xor',...
-%                 'tag', 'VERTICAL_TAG_LINE');
-%             end
-%             
-%           end
-%           
-%       end % end of switch
-%       
-%     end
-%     
-%   end
+    case 'shift'
+      
+      % set cursor to fullcross or reset to normal arrow
+      set(src, 'pointer', 'crosshair');
+      
+    case {'alt','control'}
+      
+      % get current position of cusor and return its coordinates in
+      % axes
+      a = get(obj.axes.hdlPlotAxes(1), 'CurrentPoint');
+      x = a(2,1);
+      
+      % test if cursor is inside data interval
+      if x > obj.nc.Variables.DAYD.data__(1) && x < obj.nc.Variables.DAYD.data__(end)
+        
+        % loop over 3 subplot and draw vertical lines
+        for iplot = 1:3
+          limy = get(obj.axes.hdlPlotAxes(iplot), 'YLim');
+          line(obj.axes.hdlPlotAxes(iplot), [x x], limy, 'color', 'k',...
+            'tag', 'VERTICAL_TAG_LINE');
+        end
+        
+      end
+      
+  end % end of switch
+  
+end
+
